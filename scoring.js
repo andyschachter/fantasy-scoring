caad97db00787f3e48e744ac6ed172ac98da14c9
Andy Schachter <andy.schachter@gmail.com>
2021-03-28 13:59:57 -0400

    updated function for QB test case

--- scoring.js
+++ scoring.js
@@ -1,3 +1,11 @@
-const calculateScore = () => {
+const calculateScore = (player) => {
+    switch (player.position) {
+        case 'QB':
+            score = (player.stats.passing.yards / 25) + (player.stats.passing.touchdowns * 6) + (player.stats.passing.interceptions * -3) + (player.stats.rushing.touchdowns * 6) + (player.stats.rushing.yards / 10) + (player.stats.rushing.fumbles * -3) ;
+            break;
+        default:
+            score = 0;
+    }
+    return score;
 }
 module.exports = calculateScore
